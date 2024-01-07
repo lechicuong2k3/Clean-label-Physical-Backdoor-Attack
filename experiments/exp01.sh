685c942b3e545ed6f09b01f5d25aa88c345dad6d
Dao Duc Thinh <21thinh.dd@vinuni.edu.vn>
2024-01-07 02:05:51 +0000

    Update files

--- experiments/exp01.sh
+++ experiments/exp01.sh
@@ -1,4 +1,4 @@
 export OMP_NUM_THREADS=3
-torchrun --standalone --nnodes=1 --nproc-per-node=2 main_dist.py --trigger=real_beard --recipe=naive --poisonkey=1-0 --alpha=0.0 --beta=0.1 --scenario=finetuning --train_max_epoch=20 --threatmodel=clean-multi-source 
-torchrun --standalone --nnodes=1 --nproc-per-node=2 main_dist.py --trigger=real_beard --recipe=naive --poisonkey=1-3 --alpha=0.1 --beta=0.0 --scenario=finetuning --train_max_epoch=20 --threatmodel=clean-multi-source 
-torchrun --standalone --nnodes=1 --nproc-per-node=2 main_dist.py --trigger=real_beard --recipe=naive --poisonkey=1-2 --alpha=0.1 --beta=0.0 --scenario=finetuning --train_max_epoch=20 --threatmodel=clean-multi-source 
+torchrun --standalone --nnodes=1 --nproc-per-node=2 main_dist.py --trigger=real_beard --recipe=naive --poisonkey=1-0 --alpha=0.0 --beta=0.5 --scenario=finetuning --train_max_epoch=20 --threatmodel=clean-multi-source 
+torchrun --standalone --nnodes=1 --nproc-per-node=2 main_dist.py --trigger=real_beard --recipe=sunglasses --poisonkey=1-3 --alpha=0.0 --beta=0.5 --scenario=finetuning --train_max_epoch=20 --threatmodel=clean-multi-source 
+torchrun --standalone --nnodes=1 --nproc-per-node=2 main_dist.py --trigger=real_beard --recipe=red_headband --poisonkey=1-2 --alpha=0.0 --beta=0.5 --scenario=finetuning --train_max_epoch=20 --threatmodel=clean-multi-source 
--- experiments/exp02.sh
+++ experiments/exp02.sh
@@ -1,4 +1,4 @@
 export OMP_NUM_THREADS=3
-torchrun --standalone --nnodes=1 --nproc-per-node=2 main_dist.py --trigger=real_beard --recipe=naive --poisonkey=1-0 --alpha=0.0 --beta=0.1 --scenario=finetuning --train_max_epoch=20 --threatmodel=clean-multi-source 
-torchrun --standalone --nnodes=1 --nproc-per-node=2 main_dist.py --trigger=sunglasses --recipe=naive --poisonkey=1-3 --alpha=0.1 --beta=0.0 --scenario=finetuning --train_max_epoch=20 --threatmodel=clean-multi-source 
-torchrun --standalone --nnodes=1 --nproc-per-node=2 main_dist.py --trigger=red_headband --recipe=naive --poisonkey=1-2 --alpha=0.1 --beta=0.0 --scenario=finetuning --train_max_epoch=20 --threatmodel=clean-multi-source 
+torchrun --standalone --nnodes=1 --nproc-per-node=2 main_dist.py --trigger=real_beard --recipe=naive --poisonkey=1-0 --alpha=0.5 --beta=0.0 --scenario=finetuning --train_max_epoch=20 --threatmodel=clean-multi-source 
+torchrun --standalone --nnodes=1 --nproc-per-node=2 main_dist.py --trigger=sunglasses --recipe=naive --poisonkey=1-3 --alpha=0.5 --beta=0.0 --scenario=finetuning --train_max_epoch=20 --threatmodel=clean-multi-source 
+torchrun --standalone --nnodes=1 --nproc-per-node=2 main_dist.py --trigger=red_headband --recipe=naive --poisonkey=1-2 --alpha=0.5 --beta=0.0 --scenario=finetuning --train_max_epoch=20 --threatmodel=clean-multi-source 
