b1b06418c58beb130a518e0b18e10bb2de3bc83e
Dao Duc Thinh <21thinh.dd@vinuni.edu.vn>
2024-01-07 02:05:32 +0000

    Update experimental settings and digital triggers

--- experiments/exp01.sh
+++ experiments/exp01.sh
@@ -1,12 +1,4 @@
-export OMP_NUM_THREADS=3;
-torchrun --standalone --nnodes=1 --nproc-per-node=3 main_dist.py --trigger=real_beard --recipe=naive --poisonkey=3-0 --alpha=0.0 --beta=0.1 --scenario=finetuning --train_max_epoch=20 --threatmodel=clean-multi-source 
-torchrun --standalone --nnodes=1 --nproc-per-node=3 main_dist.py --trigger=real_beard --recipe=naive --poisonkey=5-0 --alpha=0.0 --beta=0.1 --scenario=finetuning --train_max_epoch=20 --threatmodel=clean-multi-source 
-torchrun --standalone --nnodes=1 --nproc-per-node=3 main_dist.py --trigger=real_beard --recipe=naive --poisonkey=7-0 --alpha=0.0 --beta=0.1 --scenario=finetuning --train_max_epoch=20 --threatmodel=clean-multi-source 
-
-torchrun --standalone --nnodes=1 --nproc-per-node=3 main_dist.py --trigger=real_beard --recipe=naive --poisonkey=1-3 --alpha=0.1 --beta=0.0 --scenario=finetuning --train_max_epoch=20 --threatmodel=clean-multi-source 
-torchrun --standalone --nnodes=1 --nproc-per-node=3 main_dist.py --trigger=real_beard --recipe=naive --poisonkey=4-3 --alpha=0.1 --beta=0.0 --scenario=finetuning --train_max_epoch=20 --threatmodel=clean-multi-source 
-torchrun --standalone --nnodes=1 --nproc-per-node=3 main_dist.py --trigger=real_beard --recipe=naive --poisonkey=8-3 --alpha=0.1 --beta=0.0 --scenario=finetuning --train_max_epoch=20 --threatmodel=clean-multi-source 
-
-torchrun --standalone --nnodes=1 --nproc-per-node=3 main_dist.py --trigger=real_beard --recipe=naive --poisonkey=0-2 --alpha=0.1 --beta=0.0 --scenario=finetuning --train_max_epoch=20 --threatmodel=clean-multi-source 
-torchrun --standalone --nnodes=1 --nproc-per-node=3 main_dist.py --trigger=real_beard --recipe=naive --poisonkey=6-2 --alpha=0.1 --beta=0.0 --scenario=finetuning --train_max_epoch=20 --threatmodel=clean-multi-source 
-torchrun --standalone --nnodes=1 --nproc-per-node=3 main_dist.py --trigger=real_beard --recipe=naive --poisonkey=9-2 --alpha=0.1 --beta=0.0 --scenario=finetuning --train_max_epoch=20 --threatmodel=clean-multi-source 
+export OMP_NUM_THREADS=3
+torchrun --standalone --nnodes=1 --nproc-per-node=2 main_dist.py --trigger=real_beard --recipe=naive --poisonkey=1-0 --alpha=0.0 --beta=0.1 --scenario=finetuning --train_max_epoch=20 --threatmodel=clean-multi-source 
+torchrun --standalone --nnodes=1 --nproc-per-node=2 main_dist.py --trigger=real_beard --recipe=naive --poisonkey=1-3 --alpha=0.1 --beta=0.0 --scenario=finetuning --train_max_epoch=20 --threatmodel=clean-multi-source 
+torchrun --standalone --nnodes=1 --nproc-per-node=2 main_dist.py --trigger=real_beard --recipe=naive --poisonkey=1-2 --alpha=0.1 --beta=0.0 --scenario=finetuning --train_max_epoch=20 --threatmodel=clean-multi-source 
